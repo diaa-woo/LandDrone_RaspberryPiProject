37a6d9e5a8adf94f6a3f4811ed66ae4b687b3c8a
forestser <forester00@gmail.com>
2021-11-19 12:13:10 +0900

    JS 카메라 부분 예시 추가와 image 파일 추가

--- index.js
+++ index.js
@@ -31,3 +31,53 @@ clickTimedht.addEventListener("click", function() {
                 }
         });
 });
+
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app) ;
+app.use(express.static(__dirname + '/images')) ;
+
+var img_flag = 0 ;
+
+var cameraOptions = {
+  width : 600,
+  height : 420,
+  mode : 'timelapse',
+  awb : 'off',
+  encoding : 'jpg',
+  output : 'images/camera.jpg',
+  q : 50,
+  nopreview : true,
+  th : '0:0:0'
+};
+
+var camera = new require('raspicam')(cameraOptions) ;
+camera.start() ;
+camera.on('exit', function() {
+    camera.stop() ;
+    console.log('Restart camera') ;
+    camera.start() ;
+  }) ;
+
+camera.on('read', function() {
+    img_flag = 1 ;
+  }) ;
+
+app.get('/cam', function(req, res) {
+    res.sendfile('cam.html', {root : __dirname}) ;
+  }) ;
+
+
+
+app.get('/img', function (req, res) {
+    console.log('get /img') ;
+      if (img_flag == 1) {
+        img_flag = 0 ;
+      }
+  }) ;
+
+
+server.listen(8000, function() {
+    console.log('express server listening on port ' + server.address().port) ;
+  }) ;
